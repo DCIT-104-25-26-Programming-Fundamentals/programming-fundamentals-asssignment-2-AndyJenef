1c204d4d52336bd01c954ad18c49878f32729657
AJ <andyjenef@gmail.com>
2026-07-28 00:41:39 +0000

    Unbothered

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,24 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+	if (n < 2) return false;
+	if (n == 2) return true;
+	if (n % 2 == 0) return false;
+	for (int i = 3; i * 1LL * i <= n; i += 2) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
+int main() {
+	cout << "Enter a number: ";
+	int num;
+	if (!(cin >> num)) return 0;
+	if (isPrime(num))
+		cout << num << " is a prime number." << endl;
+	else
+		cout << num << " is NOT a prime number." << endl;
+	return 0;
+}
+
